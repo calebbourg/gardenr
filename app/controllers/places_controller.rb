b2dc740816843f37cd306739fe7ae942648abe7e
Caleb <calebbourg2@gmail.comm>
2015-10-29 16:36:00 +0000

    routes for simple_form

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,9 +1,19 @@
 class PlacesController < ApplicationController
 	def index
-		@places = Place.all
+		@places = Place.all.page(params[:page]).per(10)
 	end
 def new
 	@place = Place.new
+end
+def create
+	Place.create(place_params)
+	redirect_to root_path
+end	
+
+private
+
+def place_params
+	params.require(:place).permit(:name, :description, :address)
 end
 end
 
