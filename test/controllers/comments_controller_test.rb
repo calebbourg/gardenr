d53ab795e2bfa4e5d03cf755953eb71ffb618331
Caleb <calebbourg2@gmail.comm>
2016-04-19 22:23:43 +0000

    add better picture

--- test/controllers/comments_controller_test.rb
+++ test/controllers/comments_controller_test.rb
@@ -1,21 +1,20 @@
 require 'test_helper'
 
 class CommentsControllerTest < ActionController::TestCase
-test "create" do 
-    user= FactoryGirl.create(:user)
+  test "create" do 
+    user = FactoryGirl.create(:user)
     sign_in user
-  		place = FactoryGirl.create(:place)
+    place = FactoryGirl.create(:place)
 
-		  	assert_difference 'Comment.count' do
-		  		post :create, :place_id => place.id, {:comment => { 
-		  			:message => 'what is this place?', 
-		  			:rating => '1_star',
-		  			}
-		  		}
-		  	end 
+    assert_difference 'Comment.count' do
+      post :create, :place_id => place.id,  :comment => { 
+          :message => 'what is this place?', 
+          :rating => '1_star',
+        }
+      
+    end # end # assert_difference ...
 
-  	assert_redirected_to place_path
-  	assert_equal 1, user.places.comments.count
-	end 
-end
-
+    assert_redirected_to place_path(place)
+    assert_equal 1, user.comments.count
+  end # end # the test "create" do
+end 
